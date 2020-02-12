2fe6718af83e63c9d9498db26a531c7fdfe59373
eduardo <ehdiniz23@gmail.com>
2020-02-12 04:39:28 -0300

    Adicionei selects novos

--- index.php
+++ index.php
@@ -9,107 +9,138 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script> 
-    <script src="jquery.min.js"></script>
     <script type="text/javascritp" src="script.js"></script>
-    <title>Formulário</title>
+    <title>Questionário de Indicadores</title>
     <!-- PRIMEIRO SELECT -->
 <?php
 include('conexao.php');
-$sql = $conn->query("select * from categoria order by nome ");
+$sql = $conn->query("select * from tb_indicadores group by up  ");
 ?>  
 
 <!-- SCRIPT DO AJAX/JQUERY -->
-<script type="text/javascript">
+< <script type="text/javascript">
 $(document).ready(function(){
-    $('#cat').on('change',function(){
-        var catID = $(this).val();
-        if(catID){
+    $('#listaUP').on('change',function(){
+        var codID = $(this).val();
+        if(codID){
             $.ajax({
                 type:'POST',
                 url:'dados.php',
-                data:'cat_id='+catID,
+                data:'listaUP_id='+codID,
                 success:function(html){
-                    $('#sub').html(html);
+                    $('#listaPrg').html(html);
                 }
             }); 
         }else{
-            $('#sub').html('<option value="">Selecione</option>'); 
+            $('#listaPrg').html('<option value="">Select country first</option>'); 
         }
     });
-
+    
+    $('#listaPrg').on('change',function(){
+        var prgID = $(this).val();
+        if(prgID){
+            $.ajax({
+                type:'POST',
+                url:'dados.php',
+                data:'listaPrg_id='+prgID,
+                success:function(html){
+                $('#listaAction').html(html);
+                }
+            }); 
+        }else{
+            $('#listaAction').html('<option value="">Selecione</option>');
+        }
+    });
 });
 </script>
-
 </head>
-
 <body>
+
     <div class="primary-div">
-        <form method="POST" action="dados.php" name="formulario1" class="form1">
+    <form method="POST" action="dados.php" name="formulario1" class="form1">
 
-            <h1>FORMULÁRIO</h1>
+            <h1>Questionário COMAV</h1>
+		
             <div class="container">
-                <label for="">Nome</label>
-                <input type="text" name="name" class="select"><br>
-            </div>
-
-            <div class="container">
-                <label for="">SELECT 1(Departamento)</label>
-                <select id="cat" name="cat" class="select1">
-                    <option value="">Selecione</option>
+                <label for="">Órgão</label>
+          
+				<select id="listaUP" name="orgao" class="select1" >
+                <option value="">Selecione</option>
                 <?php
-    
-                    while($row = mysqli_fetch_assoc($sql)){
-                        echo '<option value="'.$row['id'].'">'.$row['nome'].'</option>';
-                    }
-                ?> 
-                </select> <br>    	 
-            </div>
-            <div class="container">
-                <label for="">SELECT 2</label>
-                <select class="select1" name="sub" id="sub">
-                    <option value="-">Selecione</option>
-                </select> 
-            </div> 
 
-            <div class="container">
-                <label for="">data resposta</label>
-                <input type="date" name="dtres" class="select"> <br>
+                while($row = mysqli_fetch_assoc($sql)){
+                    echo '<option value="'.$row['codigo_up'].'">'.$row['up'].'</option>';
+                }
+
+                ?>
+				</select>
+				
             </div>
 
             <div class="container">
-                <label for=""> quantidade Realizada</label>
-                <input type="number" name="qtreali" class="select"> <br>
-            </div>
-
-            <div class="container">
-                <label for="">quantidade Real </label>
-                <input type="number" name="qtreal" class="select"> <br>
-            </div>
-
-            <div class="container">
-                <label for="">valor Provisionado</label>
-                <input type="number" name="valorP" class="select"> <br>
-            </div>
-
-            <div class="container">
-                <label for="">valor Utilizado</label>
-                <input type="number" name="valorU" class="select"> <br>
-            </div>
-
-            <div class="container">
-                <label for="">Resultado Final</label>
-                <input type="text" name="rfinal" class="select"> <br>
+                <label for="">Programa</label>
+                
+                <select id="listaPrg" name="programa" class="select1" >
+                    <option value="">Selecione</option>
+                </select>
 
             </div>
-            <div class="container">
-                <label for="">Observacao</label>
-                <input type="text" textarea="30" name="obs" class="select">
+			
+			 <div class="container">
+                <label for="">Ação</label>         
+				<select id="listaAction" name="acao" class="select1">
+
+                </select> <br>
+		
             </div>
+
+			<!-- REALIZAR UMA CONSULTA NA TABELA DE INDICADORES -->
+			
+			 <div class="container">
+                <label for="">Indicadores</label>         
+				 <select id="listaInd" name="indicador" class="select1" >
+						
+					
+  
+                    </select> <br>
+		
+            </div>
+			
+		
+            <div class="container">
+                <label for="">Realizado</label>
+                <input type="number" name="realizado" class="select"> <br>
+            </div>
+        
+
+            <div class="container">
+                <label for="">Observação</label>
+                <input type="text" name="obs" class="select"> <br>
+
+            </div>
+			
+			   <div class="container">
+                <label for="">Data Resposta</label>
+                <input type="date" name="dtresposta" class="select"> <br>
+            </div>
+			
+			  <div class="container">
+                <label for="">Nome do Respondente</label>
+                <input type="text" name="name" class="select"> <br>
+            </div>
+       
             <center>
-                <input type="submit" class="button">
+				<input type="submit" class="button">
+				
             </center>
+			
         </form>
     </div>
+	
+	 <script>   
+
+    </script>
+	
 </body>
 
 </htm
