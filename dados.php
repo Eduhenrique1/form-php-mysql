02130f7abfcc2d590e26a852165822caa120d25a
eduardo <ehdiniz23@gmail.com>
2020-02-12 04:37:28 -0300

    Adicionei configurações dos menus dois e tres

--- dados.php
+++ dados.php
@@ -1,15 +1,14 @@
 
 <!-- FAZENDO INSERT NO MYSQL -->
 <?php
-$nome = $_POST["name"];
-$departamento = $_POST["cat"];
-$dataResposta = $_POST["dtres"];
-$quantidadeRealizada = $_POST["qtreali"];
-$quantidadeReal = $_POST["qtreal"];
-$valorProvisionado = $_POST["valorP"];
-$valorUtilizado = $_POST["valorU"];
-$Resultado = $_POST["rfinal"];
-$Observacao = $_POST["obs"];
+$orgao = $_POST["orgao"];
+$programa = $_POST["programa"];
+$acao = $_POST["acao"];
+$indicador = $_POST["indicador"];
+$realizado = $_POST["realizado"];
+$observacao = $_POST["obs"];
+$data_resposta = $_POST["dtresposta"];
+$name = $_POST["name"];
 
 $servername    = "localhost"; 
 $user = "root"; 
@@ -23,7 +22,7 @@ if ($conn->connect_error) {
     die("Sem acesso ao Banco" . $conn->connect_error);
 };
 
-$sql= "INSERT INTO tbquestionario(Nome,departamento, dataResposta, quantidadeRealizada, quantidadeReal, valorProvisionado, valorUtilizado,ResultadoFinal, Observacao) VALUES ('$nome','$departamento','$dataResposta','$quantidadeRealizada','$quantidadeReal','$valorProvisionado','$valorUtilizado','$Resultado','$Observacao')";
+$sql= "insert into tbquestionario(orgao, programa, acao, indicador, vl_realizado, observacao, dataResposta, nomeRespondente) values ('$orgao','$programa','$acao','$indicador','$realizado','$observacao','$data_resposta','$name')";
 
 if($conn->query($sql) == TRUE) {
 } else {
@@ -35,16 +34,24 @@ $conn->close();
 
 <!-- FAZENDO UMA QUERY NO MYSQL PARA O SEGUNDO SELECT  -->
 <?php 
-//Include database configuration file
 include('conexao.php');
 
-if(isset($_POST["cat_id"]) && !empty($_POST["cat_id"])){
-    //Get all state data
-    $query = $conn->query("SELECT * FROM sub WHERE id_cat= ".$_POST['cat_id']." order by nome_sub ");
-        while($row = $query->fetch_assoc()){ 
-            echo '<option value="'.$row['id_cat'].'">'.$row['nome_sub'].'</option>';
+if(isset($_POST["listaUP_id"]) && !empty($_POST["listaUP_id"])){
+    
+    $query = $conn->query("select * from tb_indicadores where codigo_up = ".$_POST['listaUP_id']." group by programa");
+    while($row = $query->fetch_assoc() or die($conn->error)){  
+            echo '<option value="'.$row['codigo_programa'].'">'.$row['programa'].'</option>';
         }
     }
+
+    if(isset($_POST["listaPrg_id"]) && !empty($_POST["listaPrg_id"])){
+        //Get all city data
+        $query = $conn->query("select * from tb_indicadores where codigo_programa = ".$_POST['listaPrg_id']." group by acao");  
+        while($row = $query->fetch_assoc() or die($conn->error)){ 
+                echo '<option value="'.$row['codigo_acao'].'">'.$row['acao'].'</option>';
+            }
+        }
+
 
 ?>
 <!DOCTYPE html>
